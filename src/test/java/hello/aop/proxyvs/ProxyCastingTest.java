4ebe1828b02d307c9008ee2d067bdae0c3bf6bc8
jongyounlee <jayylee.dev@gmail.com>
2023-11-02 15:55:04 +0900

    13

---
+++ src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,17 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+    }
+}
